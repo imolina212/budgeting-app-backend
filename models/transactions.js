bd110d4425dbf50cda4f78c483ff569bcf73302d
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-18 16:12:52 -0500

    added seed data

---
+++ models/transactions.js
@@ -0,0 +1,63 @@
+
+module.exports = [
+    {
+      date: "April 1",
+      source: "Income",
+      amount: 1000,
+      from: "Employer",
+    },
+    {
+      date: "April 1",
+      source: "Taxes",
+      amount: -300,
+      from: "Uncle-Sam",
+    },
+    {
+      date: "April 1",
+      source: "Retirement",
+      amount: -200,
+      from: "Employer",
+    },
+    {
+      date: "April 1",
+      source: "Savings",
+      amount: -100,
+      from: "Bank",
+    },
+    {
+      date: "April 1",
+      source: "Credit Card Payment",
+      amount: -100,
+      from: "Bank",
+    },
+    {
+      date: "April 5",
+      source: "Monthly Birthday Money from Aunt Tilda",
+      amount: 20,
+      from: "Bank",
+    },
+    {
+      date: "April 5",
+      source: "Coffee",
+      amount: -4,
+      from: "Grocery-Store",
+    },
+    {
+      date: "April 5",
+      source: "Internet",
+      amount: -100,
+      from: "Internet Service Provider",
+    },
+    {
+      date: "April 3",
+      source: "Groceries",
+      amount: -76,
+      from: "Grocery-Store",
+    },
+    {
+      date: "April 3",
+      source: "Pet Food",
+      amount: -7,
+      from: "Pet-Store",
+    },
+  ];
